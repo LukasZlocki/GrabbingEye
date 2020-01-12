1dd70178390fb87100102b79e02a541a83cd0be7
lzlocki <lzlocki@poczta.fm>
2020-01-12 10:45:55 +0100

    adding htmlAgilityPack and copy MainWindow code

--- GrabbingEye/MainWindow.xaml.cs
+++ GrabbingEye/MainWindow.xaml.cs
@@ -13,16 +13,87 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GrabbingEye
+using HtmlAgilityPack;
+
+using SludgeProCreator.Models;
+using SludgeProCreator.ModelsSql;
+
+namespace SludgeProCreator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        YearlyFinancialRaportStandard finansialRaport = new YearlyFinancialRaportStandard();
+        StocksPolishCompany stocksPolishCompany = new StocksPolishCompany();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region Buttons
+
+        // GET financial raport basis on _stockName and _raportYear
+        private void btnextract_Click(object sender, RoutedEventArgs e)
+        {
+            string _stockName = txtStockName.Text;
+            int _raportYear = Convert.ToInt32(txtRaportYear.Text);
+
+            DataSniffer SniffFor = new DataSniffer(_stockName, _raportYear);
+
+            // GET data from web 
+            finansialRaport = SniffFor.GetFinancialRaport();
+
+            // Showing data on screen
+            ShowFinancialRaportOnScreen(finansialRaport);
+        }
+
+        // GET all stock companies at sql database
+        private void btServerOn_Click(object sender, RoutedEventArgs e)
+        {
+            // Todo : code connection to server
+            SqlAdapter SqlAdapt = new SqlAdapter();
+        }
+
+        // GET all companies from Sql and store them in StockPolishcompany list
+        private void btGetAllCompaniesFromSql_Click(object sender, RoutedEventArgs e)
+        {
+            SqlAdapter SqlAdapt = new SqlAdapter();
+            SqlAdapt.GetPolishCompanies();
+        }
+
+        #endregion
+
+        #region Show Data on screen
+        // SHOW - financial Raport
+        private void ShowFinancialRaportOnScreen(YearlyFinancialRaportStandard raport)
+        {
+            txtBox.Text =
+                "Nazwa spolki : " + finansialRaport.Name + " \n " +
+                "Rok Raportu : " + finansialRaport.Year + " \n " +
+                "Przychody netto ze sprzedazy : " + finansialRaport.PrzychodyNettoZeSprzedazy + " \n " +
+                "Zyski z dzialalnosci operacyjnej : " + finansialRaport.ZyskDzialalnosciOperacyjnej + " \n " +
+                "Zysk brutto : " + finansialRaport.ZyskBrutto + " \n " +
+                "Zysk netto : " + finansialRaport.ZyskNetto + " \n " +
+                "Amortyzacja : " + finansialRaport.Amortyzacja + " \n " +
+                "Ebitda : " + finansialRaport.Ebitda + " \n " +
+                "Aktywa : " + finansialRaport.Aktywa + " \n " +
+                "Kapital wlasny : " + finansialRaport.KapitalWlasny + " \n " +
+                "Liczba Akcji : " + finansialRaport.LiczbaAkcji + " \n " +
+                "Zysk na akcje : " + finansialRaport.ZyskNaAkcje + " \n " +
+                "Wartosc ksiegowa na akcje : " + finansialRaport.WartoscKsiegowaNaAkcje + " \n ";
+
+        }
+
+
+
+
+
+        #endregion
+
+
     }
 }
