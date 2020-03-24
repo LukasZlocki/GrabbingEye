d3f02a97eb3ffbea92e9f6c83009f7a0b83ac153
lzlocki <lzlocki@poczta.fm>
2020-03-24 22:59:27 +0100

    add yearly full raport model

---
+++ GrabbingEye/Models/YearlyFinancialRaportFull.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrabbingEye.Models
+{
+    class YearlyFinancialRaportFull
+    {
+
+
+        #region Rachunek zyskow i strat
+            public int PrzychodyZeSprzedazy { get; set; }
+            public int ZyskZeSprzedazy { get; set; }
+            public int ZyskOperacyjny { get; set; }       
+            public int ZyskZDzialalnosciGospodarczej { get; set; }
+            public int ZyskPrzedOpodatkowaniem { get; set; }
+            public int ZyskNetto { get; set; }
+        #endregion
+
+        #region Bilans
+            public int AktywaTwale { get; set; }      
+            public int AktywaObrotowe { get; set; }
+            public int AktywaRazem { get; set; }
+            public int KapitalWlasnyAkcjonariuszyJenostkiDominujacej { get; set; }
+            public int ZobowiazanieDlugoterminowe { get; set; }
+            public int Zobowiazaniakrotkoterminowe { get; set; }
+            public int PasywaRazem { get; set; }
+        #endregion
+
+
+    }
+}
